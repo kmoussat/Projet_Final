84096dcbeac98cd381ecc515bc76ad5d0a9df62b
Ken Moussat <ken.moussat@devinci.fr>
2016-04-12 09:33:27 +0200

    Commit

--- POA_Final/POA_Final/Carre.cs
+++ POA_Final/POA_Final/Carre.cs
@@ -6,7 +6,56 @@ using System.Threading.Tasks;
 
 namespace POA_Final
 {
-    public class Carre
+    public class Carre : Element
     {
+        private Point point;
+        private double cote;
+     
+
+        public Carre(string nom, int idElement, Point point, double cote, Couleur couleur, int ordre) : base(nom, idElement, couleur, ordre)
+        {
+            this.point = point;
+            this.cote = cote;
+          
+        }
+
+        public void Translater(double dx, double dy)
+        {
+            this.DX = dx;
+            this.DY = dy;
+        }
+
+        public void Rotation(double alpha, double cx, double cy)
+        {
+            this.Alpha = alpha;
+            this.CX = cx;
+            this.CY = cy;
+        }
+
+        public override string ToString()
+        {
+            if (this.DX == 0 && this.DY == 0)
+            {
+                if (this.Alpha == 0 && this.CX == 0 && this.CY == 0)
+                {
+                    return "<rect x=\"" + point.X + "\" y=\"" + point.Y + "\" width=\"" + cote + "\" height=\"" + cote + "\" style=\"fill :rgb(" + Couleur + ")\" />";
+                }
+                else
+                {
+                    return "<rect x=\"" + point.X + "\" y=\"" + point.Y + "\" width=\"" + cote + "\" height=\"" + cote + "\" style=\"fill :rgb(" + Couleur + ")\"" + " transform=\"rotate(" + this.Alpha + " " + this.CX + "," + this.CY + ")\"/>";
+                }
+            }
+            else
+            {
+                if (this.Alpha == 0 && this.CX == 0 && this.CY == 0)
+                {
+                    return "<rect x=\"" + point.X + "\" y=\"" + point.Y + "\" width=\"" + cote + "\" height=\"" + cote + "\" style=\"fill :rgb(" + Couleur + ")\"" + " transform=\"translate(" + this.DX + "," + this.DY + ")\"/>";
+                }
+                else
+                {
+                    return "<rect x=\"" + point.X + "\" y=\"" + point.Y + "\" width=\"" + cote + "\" height=\"" + cote + "\" style=\"fill :rgb(" + Couleur + ")\"" + " transform=\"rotate(" + this.Alpha + " " + this.CX + "," + this.CY + ") translate(" + this.DX + "," + this.DY + ")\" />";
+                }
+            }
+        }
     }
 }
