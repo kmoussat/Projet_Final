52d751ce0c86055037c2c2cb28ed31185af7b178
Ken Moussat <ken.moussat@devinci.fr>
2016-04-12 09:14:56 +0200

    C#

--- POA_Final/POA_Final/Cercle.cs
+++ POA_Final/POA_Final/Cercle.cs
@@ -3,10 +3,59 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace POA_Final
 {
-    class Cercle
+    class Cercle : Element
     {
+        private Point centre;
+        private double r;
+
+        public Cercle(string nom, int idElement, Point centre, double r, Couleur couleur, int ordre) : base(nom, idElement, couleur, ordre)
+        {
+            this.centre = centre;
+            this.r = r;
+        }
+
+        public void Translater(double dx, double dy)
+        {
+            this.DX = dx;
+            this.DY = dy;
+        }
+
+        public void Rotation(double alpha, double cx, double cy)
+        {
+            this.Alpha = alpha;
+            this.CX = cx;
+            this.CY = cy;
+        }
+
+        public override string ToString()
+        {
+            if (this.DX == 0 && this.DY == 0)
+            {
+                if (this.Alpha == 0 && CX == 0 && CY == 0)
+                {
+                    return "<circle cx=\"" + centre.X + "\" cy=\"" + centre.Y + "\" r=\"" + r + "\" style=\"fill :rgb(" + Couleur + ")\" />";
+                }
+                else
+                {
+                    return "<circle cx=\"" + centre.X + "\" cy=\"" + centre.Y + "\" r=\"" + r + "\" style=\"fill :rgb(" + Couleur + ")\"" + "transform =\"rotate(" + this.Alpha + " " + this.CX + "," + this.CY + ")\" />";
+                }
+            }
+            else
+            {
+                if (this.Alpha == 0 && CX == 0 && CY == 0)
+                {
+                    return "<circle cx=\"" + centre.X + "\" cy=\"" + centre.Y + "\" r=\"" + r + "\" style=\"fill :rgb(" + Couleur + ")\"" + "transform =\"translate(" + this.DX + "," + this.DY + ")\" />";
+                }
+                else
+                {
+                    return "<circle cx=\"" + centre.X + "\" cy=\"" + centre.Y + "\" r=\"" + r + "\" style=\"fill :rgb(" + Couleur + ")\"" + "transform=\"rotate(" + this.Alpha + " " + this.CX + "," + this.CY + ") translate(" + this.DX + "," + this.DY + ")\" />";
+                }
+            }
+        }
+
     }
 }
--- POA_Final/POA_Final/Chemin.cs
+++ POA_Final/POA_Final/Chemin.cs
@@ -3,10 +3,57 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace POA_Final
 {
-    class Chemin
+    class Chemin : Element
     {
+        private string path;
+
+        public Chemin(string nom, int idElement, string path, Couleur couleur, int ordre) : base(nom, idElement, couleur, ordre)
+        {
+            this.path = path;
+        }
+
+        public void Translater(double dx, double dy)
+        {
+            this.DX = dx;
+            this.DY = dy;
+        }
+
+        public void Rotation(double alpha, double cx, double cy)
+        {
+            this.Alpha = alpha;
+            this.CX = cx;
+            this.CY = cy;
+        }
+
+        public override string ToString()
+        {
+            if (this.DX == 0 && this.DY == 0)
+            {
+                if (this.Alpha == 0 && CX == 0 && CY == 0)
+                {
+                    return "<path d=\"" + path + "\" style=\"fill :rgb(" + Couleur + ")\" />";
+                }
+                else
+                {
+                    return "<path d=\"" + path + "\" style=\"fill :rgb(" + Couleur + ")\"" + " transform=\"rotate(" + this.Alpha + " " + this.CX + "," + this.CY + ")\" />";
+                }
+            }
+            else
+            {
+                if (this.Alpha == 0 && CX == 0 && CY == 0)
+                {
+                    return "<path d=\"" + path + "\" style=\"fill :rgb(" + Couleur + ")\"" + " transform=\"translate(" + this.DX + "," + this.DY + ")\" />";
+                }
+                else
+                {
+                    return "<path d=\"" + path + "\" style=\"fill :rgb(" + Couleur + ")\"" + " transform=\"rotate(" + this.Alpha + " " + this.CX + "," + this.CY + ") translate(" + this.DX + "," + this.DY + ")\" />";
+                }
+            }
+        }
     }
 }
+
