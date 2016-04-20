227b26e6121d1787df6caab60d9c26bcf8abf5cd
Ken Moussat <ken.moussat@devinci.fr>
2016-04-20 13:41:20 +0200

    Update Polygone.cs

--- POA_Final/POA_Final/Polygone.cs
+++ POA_Final/POA_Final/Polygone.cs
@@ -7,28 +7,34 @@ using System.IO;
 
 namespace POA_Final
 {
+    //Classe héritage Element
     public class Polygone : Element
     {
         private string points;
 
+//Constructeur Polygone par des points
         public Polygone(string nom, int idElement, string points, Couleur couleur, int ordre) : base(nom, idElement, couleur, ordre)
         {
             this.points = points;
         }
 
+// Méthode de translation des points
         public void Translater(double dx, double dy)
         {
             this.DX = dx;
             this.DY = dy;
         }
 
+// Méthode de rotation des points
         public void Rotation(double alpha, double cx, double cy)
         {
             this.Alpha = alpha;
             this.CX = cx;
             this.CY = cy;
         }
-
+        
+        
+//  Méthode de dessin qui renvoie au format svg les données csv
         public override string ToString()
         {
             if (this.DX == 0 && this.DY == 0)
