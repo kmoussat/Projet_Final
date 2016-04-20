fb75a87f06572c667e0baa20c920bae1f7a9d36f
Ken Moussat <ken.moussat@devinci.fr>
2016-04-20 13:46:34 +0200

    Update Rectangle.cs

--- POA_Final/POA_Final/Rectangle.cs
+++ POA_Final/POA_Final/Rectangle.cs
@@ -7,12 +7,15 @@ using System.IO;
 
 namespace POA_Final
 {
+    //Classe héritage Element
     public class Rectangle : Element
     {
         private Point point;
         private double largeur;
         private double hauteur;
 
+
+//Constructeur Rectangle des points, une donnée de largeur et une donnée de longueur
         public Rectangle(string nom, int idElement, Point point, double largeur, double hauteur, Couleur couleur, int ordre) : base(nom, idElement, couleur, ordre)
         {
             this.point = point;
@@ -20,12 +23,15 @@ namespace POA_Final
             this.hauteur = hauteur;
         }
 
+
+// Méthode de translation des points
         public void Translater(double dx, double dy)
         {
             this.DX = dx;
             this.DY = dy;
         }
 
+// Méthode de rotation des points
         public void Rotation(double alpha, double cx, double cy)
         {
             this.Alpha = alpha;
@@ -33,6 +39,8 @@ namespace POA_Final
             this.CY = cy;
         }
 
+
+//  Méthode de dessin qui renvoie au format svg les données csv
         public override string ToString()
         {
             if (this.DX == 0 && this.DY == 0)
