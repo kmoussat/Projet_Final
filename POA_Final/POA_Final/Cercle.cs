74096d47257cc410c255c2d64d2dd4c600d49f1b
Ken Moussat <ken.moussat@devinci.fr>
2016-04-20 13:20:10 +0200

    Update Cercle.cs

--- POA_Final/POA_Final/Cercle.cs
+++ POA_Final/POA_Final/Cercle.cs
@@ -7,30 +7,38 @@ using System.IO;
 
 namespace POA_Final
 {
+    
+    //Classe héritage Element
     public class Cercle : Element
     {
         private Point centre;
         private double r;
 
+//Constructeur Cerlce par un rayon et un point central
         public Cercle(string nom, int idElement, Point centre, double r, Couleur couleur, int ordre) : base(nom, idElement, couleur, ordre)
         {
             this.centre = centre;
             this.r = r;
         }
 
+
+// Méthode de translation des points
         public void Translater(double dx, double dy)
         {
             this.DX = dx;
             this.DY = dy;
         }
 
+// Méthode de rotation des points
         public void Rotation(double alpha, double cx, double cy)
         {
             this.Alpha = alpha;
             this.CX = cx;
             this.CY = cy;
         }
-
+        
+        
+//  Méthode de dessin qui renvoie au format svg les données csv
         public override string ToString()
         {
             if (this.DX == 0 && this.DY == 0)
