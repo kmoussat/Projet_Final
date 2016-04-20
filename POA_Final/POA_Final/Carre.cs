f4486cddf788f74e658eee77a72f2540f40471ac
Blackjeranium <paul.antonin.puech@gmail.com>
2016-04-20 13:17:10 +0200

    comment_1
    premiers commentaires

--- POA_Final/POA_Final/Carre.cs
+++ POA_Final/POA_Final/Carre.cs
@@ -8,17 +8,19 @@ namespace POA_Final
 {
     public class Carre : Element
     {
+        //Attributs
         private Point point;
         private double cote;
      
-
+        //Constructeur qui récupère le nom, l'ID, la couleur et l'ordre de la classe Element
         public Carre(string nom, int idElement, Point point, double cote, Couleur couleur, int ordre) : base(nom, idElement, couleur, ordre)
         {
             this.point = point;
             this.cote = cote;
           
         }
-
+        
+        #REGION - Méthodes
         public void Translater(double dx, double dy)
         {
             this.DX = dx;
@@ -31,7 +33,8 @@ namespace POA_Final
             this.CX = cx;
             this.CY = cy;
         }
-
+        
+        //La Méthode ToString est la méthode d'affichage qui utilise les attributs pour tracer l'élément
         public override string ToString()
         {
             if (this.DX == 0 && this.DY == 0)
@@ -57,5 +60,7 @@ namespace POA_Final
                 }
             }
         }
+        
+        #ENDREGION
     }
 }
