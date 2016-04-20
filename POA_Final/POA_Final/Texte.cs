c9a7380cab83ac576efa816123c91db96c937758
Ken Moussat <ken.moussat@devinci.fr>
2016-04-20 13:49:05 +0200

    Update Texte.cs

--- POA_Final/POA_Final/Texte.cs
+++ POA_Final/POA_Final/Texte.cs
@@ -7,23 +7,28 @@ using System.IO;
 
 namespace POA_Final
 {
+   
+    //Classe héritage Element
    public class Texte : Element
     {
         private Point point;
         private string contenu;
-
+        
+//Constructeur Texte par du texte et des points
         public Texte(string nom, int idElement, Point point, string contenu, Couleur couleur, int ordre) : base(nom, idElement, couleur, ordre)
         {
             this.point = point;
             this.contenu = contenu;
         }
 
+// Méthode de translation des points
         public void Translater(double dx, double dy)
         {
             this.DX = dx;
             this.DY = dy;
         }
 
+// Méthode de rotation des points
         public void Rotation(double alpha, double cx, double cy)
         {
             this.Alpha = alpha;
@@ -31,6 +36,8 @@ namespace POA_Final
             this.CY = cy;
         }
 
+
+//  Méthode de dessin qui renvoie au format svg les données csv
         public override string ToString()
         {
             if (this.DX == 0 && this.DY == 0)
