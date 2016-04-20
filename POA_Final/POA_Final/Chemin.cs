138ba110ac7f5c2432b333bf09385ed28a27defa
Blackjeranium <paul.antonin.puech@gmail.com>
2016-04-20 13:22:02 +0200

    comment
    premiers commentaires

--- POA_Final/POA_Final/Chemin.cs
+++ POA_Final/POA_Final/Chemin.cs
@@ -9,13 +9,17 @@ namespace POA_Final
 {
     public class Chemin : Element
     {
+        //Attribut
         private string path;
-
+        
+        //Constructeur
         public Chemin(string nom, int idElement, string path, Couleur couleur, int ordre) : base(nom, idElement, couleur, ordre)
         {
             this.path = path;
         }
-
+        
+        #region méthodes
+        
         public void Translater(double dx, double dy)
         {
             this.DX = dx;
@@ -28,7 +32,8 @@ namespace POA_Final
             this.CX = cx;
             this.CY = cy;
         }
-
+        
+        //Méthode ToString qui converti les attributs en balises SVG pour déssiner l'élément
         public override string ToString()
         {
             if (this.DX == 0 && this.DY == 0)
@@ -54,6 +59,7 @@ namespace POA_Final
                 }
             }
         }
+        #endregion
     }
 }
 
